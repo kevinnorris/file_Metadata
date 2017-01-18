527c4319066eab9a282d4b0b06e66f6a346308f5
Kevin <kevin.norris.90@gmail.com>
2017-01-18 03:43:54 +0000

    Added deletion of file after upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
 
@@ -9,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/file-size', upload.single('Choose File'), function (req, res, next) {
     res.status(200).json({size: req.file.size, mimetype: req.file.mimetype, encoding: req.file.encoding});
+    
+    // Delete the file after sending metadata
+    fs.unlink(path.join(__dirname, req.file.path));
 });
 
 app.listen(process.env.PORT || 8080);
